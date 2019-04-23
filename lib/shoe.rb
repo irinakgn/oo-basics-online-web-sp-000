44a98259665e33f02a4808c0a590940cd3337ed7
Irina Kagan <irinagamb@gmail.com>
2019-04-23 13:47:32 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,13 @@
 class Shoe
-def initialize(title)
-@title = title
-end
+  def initialize(title)
+    @title = title
+  end
 
-def title
-@title
-end
+  def title
+    @title
+  end
+
+  def brand=(title)
+  @title = title
+  end
 end
